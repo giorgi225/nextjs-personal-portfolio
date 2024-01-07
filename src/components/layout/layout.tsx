9d06e3dcee0d8ff9b08eaa9e1623d518a7dcda02
gigi <gigi.shalamberidze2022@gmail.com>
2024-01-07 12:44:48 +0400

    add sidebar

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,18 +1,17 @@
-// "use client";
 // Next
-// import { useState } from "react";
+import { useState } from "react";
 
 // Components
 import Sidebar from "@components/common/Sidebar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  // const [sidebarIsActive, setSidebarIsActive] = useState(true);
+  const [sidebarIsActive, setSidebarIsActive] = useState(true);
   const closeSidebar = (): void => {
-    // setSidebarIsActive((prevState) => !prevState);
+    setSidebarIsActive((prevState) => !prevState);
   };
   return (
     <>
-      {/* <Sidebar isActive={sidebarIsActive} closeSidebar={closeSidebar} /> */}
+      <Sidebar isActive={sidebarIsActive} closeSidebar={closeSidebar} />
       <div className="w-full bg-white nav h-auto rounded overflow-auto no-scrollbar px-5 md:px-10 lg:px-20">
         {/* <Header :isActive="sidebarActive" @toggleSidebar="toggleSidebar" /> */}
         {children}
