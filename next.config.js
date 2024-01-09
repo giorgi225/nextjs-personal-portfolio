ec03e2c87b9b5ccd4702446540c1db93e86cfb8d
gigi <gigi.shalamberidze2022@gmail.com>
2024-01-09 12:58:01 +0400

    remove optimizeCSS

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withNextIntl = require("next-intl/plugin")("./src/i18n.ts");
 const nextConfig = {
-  optimizeCss: true,
   images: {
     domains: ["upcdn.io"],
   },
