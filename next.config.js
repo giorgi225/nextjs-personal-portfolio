1f38a286fd4c08dfaa247ca466dc6bc66bc8c39d
gigi <gigi.shalamberidze2022@gmail.com>
2024-01-09 12:41:10 +0400

    optimize css

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const withNextIntl = require("next-intl/plugin")("./src/i18n.ts");
 const nextConfig = {
-    images: {
-        domains: ['upcdn.io']
-    }
+  optimizeCss: true,
+  images: {
+    domains: ["upcdn.io"],
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
